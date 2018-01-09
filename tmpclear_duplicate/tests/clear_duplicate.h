2cd3b9f07e979bc88515b4f217cf14f403a20266
Romeo Lossy <rlossy@z1r13p4.le-101.fr>
2018-01-09 17:37:57 +0100

    modif

---
+++ tmpclear_duplicate/tests/clear_duplicate.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   clear_duplicate.h                                .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rlossy <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/01/09 16:05:58 by rlossy       #+#   ##    ##    #+#       */
+/*   Updated: 2018/01/09 16:45:22 by rlossy      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef CLEAR_DUPLICATE_H
+# define CLEAR_DUPLICATE_H
+
+typedef struct	s_list
+{
+	int data;
+	struct s_list *next;
+}		t_list;
+
+#endif
